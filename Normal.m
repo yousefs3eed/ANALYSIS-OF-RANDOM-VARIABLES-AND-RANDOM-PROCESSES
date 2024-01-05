53a6a5e8b6bf6f8272dd7900beeef11e778f789e
yousefs3eed <116123814+yousefs3eed@users.noreply.github.com>
2024-01-05 18:19:48 +0200

    normal

--- Normal.m
+++ Normal.m
@@ -3,7 +3,7 @@ mu = -8;     % Mean
 sigma = 4 ;   % Standard deviation (square root of variance, which is 4)
 
 % Generate normally distributed data
-Y = normrnd(mu, sigma, [1, 100000]);
+Y = normrnd(mu, sigma, [1, 1000000]);
 
 % Display or save the data as needed
 disp(Y);
